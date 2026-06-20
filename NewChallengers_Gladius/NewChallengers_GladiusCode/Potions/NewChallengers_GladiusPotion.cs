53265e658a97cce6a7335d02db02dcc02ef51542
OnionTree <lesstory01@gmail.com>
2026-06-20 15:46:05 +0900

    add git
    add

---
+++ NewChallengers_Gladius/NewChallengers_GladiusCode/Potions/NewChallengers_GladiusPotion.cs
@@ -0,0 +1,8 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Utils;
+using NewChallengers_Gladius.NewChallengers_GladiusCode.Character;
+
+namespace NewChallengers_Gladius.NewChallengers_GladiusCode.Potions;
+
+[Pool(typeof(NewChallengers_GladiusPotionPool))]
+public abstract class NewChallengers_GladiusPotion : CustomPotionModel;
